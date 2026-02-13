516293ba6981bf7f084c031efa1799ffd25d6575
Alfredo Marvulle <alfredo.marvulle@gmail.com>
2026-02-13 18:35:38 -0300

    feat: add fontawesome icons on navigation

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -1,22 +1,28 @@
 <template>
-    <header class="stick top bg-weather-primary shadow-lg">
-        <nav class="container mx-auto flex flex-col sm:flex-row items-center gap-4 text-white py-6">
-            <RouterLink :to="{name: 'home'}">
-                <div class="flex items-center gap-3">
-                    [icon sol]
-                    <h1 class="text-2xl my-0">The Local Weather</h1>
-                </div>
-            </RouterLink>
+  <header class="stick top bg-weather-primary shadow-lg">
+    <nav
+      class="container mx-auto flex flex-col sm:flex-row items-center gap-4 text-white py-6"
+    >
+      <RouterLink :to="{ name: 'home' }">
+        <div class="flex items-center gap-3">
+          <font-awesome-icon icon="fa-regular fa-sun" />
 
-            <div class="flex gap-3 flex-1 justify-end">
-                <p class="hover:text-weather-secondary duration-150 cursor-pointer">i</p>
-                <p class="hover:text-weather-secondary duration-150 cursor-pointer">+</p>
-            </div>
-        </nav>
+          <h1 class="text-2xl my-0">The Local Weather</h1>
+        </div>
+      </RouterLink>
 
-    </header>
+      <div class="flex gap-3 flex-1 justify-end">
+          <p class="hover:text-weather-secondary duration-150 cursor-pointer">
+            <font-awesome-icon icon="fa-solid fa-circle-info" />
+        </p>
+        <p class="hover:text-weather-secondary duration-150 cursor-pointer">
+            <font-awesome-icon icon="fa-solid fa-circle-plus" />
+        </p>
+      </div>
+    </nav>
+  </header>
 </template>
 
 <script setup>
-import { RouterLink } from 'vue-router';
+import { RouterLink } from "vue-router";
 </script>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,16 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from "./router";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSun } from '@fortawesome/free-regular-svg-icons'
+import { faCirclePlus, faCircleInfo } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faSun, faCirclePlus, faCircleInfo);
 
 const app = createApp(App);
 
 app.use(router);
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.mount("#app");
